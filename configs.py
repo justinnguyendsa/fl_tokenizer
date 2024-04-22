496c96abb26e71f35d452905df97231c8e716438
Justin Nguyen <justinnguyen.dsa@gmail.com>
2024-04-22 17:03:34 +0700

    Optimize functions: bundle_score and tokenizer

--- configs.py
+++ configs.py
@@ -64,130 +64,85 @@ def recursive_bundle_structure(bundles, level=2, position='first'):
 
 def bundle_score(bundles, threshold):
   bundle_score = {}
-  for w1 in bundles.keys():
-    w1_total = bundles[w1]['total']
-    w1_main = bundles[w1]['main']
-    w1_children = bundles[w1]['children']
+  for w1, w1_data in bundles.items():
+    w1_total = w1_data['total']
+    w1_children = w1_data['children']
 
-    f2_neg = 0
-    f2_f3 = 0
-    f2_f4 = 0
-    for w2 in w1_children.keys():
-      w2_total = w1_children[w2]['total']
-      w2_main = w1_children[w2]['main']
-      w2_children = w1_children[w2]['children']
+    f2_neg, f2_f3, f2_f4 = 0, 0, 0
+    for w2, w2_data in w1_children.items():
+      w2_total = w2_data['total']
+      w2_children = w2_data['children']
 
-      f3_neg = 0
-      f3_f4 = 0
-      for w3 in w2_children.keys():
-        w3_total = w2_children[w3]['total']
-        w3_main = w2_children[w3]['main']
-        w3_children = w2_children[w3]['children']
-        
+      f3_neg, f3_f4 = 0, 0
+      for w3, w3_data in w2_children.items():
+        w3_total = w3_data['total']
+        w3_children = w3_data['children']
+
         f4_neg = 0
-        for w4 in w3_children.keys():
-          w4_total = w3_children[w4]['total']
-          w4_main = w3_children[w4]['main']
-          f4 = w4_total / w1_total
+        for w4, w4_data in w3_children.items():
+          w4_total = w4_data['total']
 
+          f4 = w4_total / w1_total
           if f4 > threshold:
-            if w4 not in bundle_score.keys():
-              bundle_score[w4] = {}
-            bundle_score[w4]['count'] = w4_total
-            bundle_score[w4]['pct'] = f4
+            bundle_score.setdefault(w4, {'count': w4_total, 'pct': f4})
             f4_neg += f4
 
         f3 = (w3_total / w1_total) - f4_neg
         if f3 > threshold:
-          if w3 not in bundle_score.keys():
-            bundle_score[w3] = {}
-          bundle_score[w3]['count'] = w3_total
-          bundle_score[w3]['pct'] = f3
+          bundle_score.setdefault(w3, {'count': w3_total, 'pct': f3})
           f3_neg += f3
         f3_f4 += f4_neg
-      
+
       f2 = (w2_total / w1_total) - f3_neg - f3_f4
       if f2 > threshold:
-        if w2 not in bundle_score.keys():
-          bundle_score[w2] = {}
-        bundle_score[w2]['count'] = w2_total
-        bundle_score[w2]['pct'] = f2
+        bundle_score.setdefault(w2, {'count': w2_total, 'pct': f2})
         f2_neg += f2
       f2_f3 += f3_neg
       f2_f4 += f3_f4
 
     f1 = 1 - f2_neg - f2_f3 - f2_f4
     if f1 > threshold:
-      if w1 not in bundle_score.keys():
-        bundle_score[w1] = {}
-      bundle_score[w1]['count'] = w1_total
-      bundle_score[w1]['pct'] = f1
+      bundle_score.setdefault(w1, {'count': w1_total, 'pct': f1})
+
   return bundle_score
 
 def tokenizer(conv_ls, token_ls):
   f_tokens_dict = {}
   l_tokens_dict = {}
   for token in token_ls:
-    m = str(token).split()
-    xf = m[0]
-    xl = m[-1]
+    m = token.split()
+    xf, xl = m[0], m[-1]
     y = len(m)
     
-    if xf not in f_tokens_dict.keys():
-      f_tokens_dict[xf] = {1: [], 2: [], 3: [], 4: []}
-    f_tokens_dict[xf][y].append(token)
-    
-    if xl not in l_tokens_dict.keys():
-      l_tokens_dict[xl] = {1: [], 2: [], 3: [], 4: []}
-    l_tokens_dict[xl][y].append(token)
-        
+    f_tokens_dict.setdefault(xf, {1: [], 2: [], 3: [], 4: []})[y].append(token)
+    l_tokens_dict.setdefault(xl, {1: [], 2: [], 3: [], 4: []})[y].append(token)
+  
   final = {}
-  for index in range(len(conv_ls)):
-    ls = conv_ls[index]
-    
+  for index, ls in enumerate(conv_ls):
     f_result = []
-    for s in ls:
-      w = s.split()
-      for i in range(len(w)):
-        z = 1
-        d = {}
-        for j in [4, 3, 2, 1]:
-          # b = tokens_dict[w[i]][j]
-          if w[i] in f_tokens_dict.keys():
-            b = f_tokens_dict[w[i]][j]
-            if len(b)> 0:
-              for k in b:
-                if str(s).find(str(k)) >= 0:
-                  d[z] = k 
-                  z = 0
-          else:
-            d[z] = w[i]
-        if 1 in d.keys():
-          f_result.append(d[1])
-    
     l_result = []
     for s in ls:
-      w = s.split()
-      for i in range(len(w)):
-        z = 1
-        d = {}
-        for j in [4, 3, 2, 1]:
-          # b = tokens_dict[w[i]][j]
-          if w[i] in l_tokens_dict.keys():
-            b = l_tokens_dict[w[i]][j]
-            if len(b)> 0:
-              for k in b:
-                if str(s).find(str(k)) >= 0:
-                  d[z] = k 
-                  z = 0
-          else:
-            d[z] = w[i]
-        if 1 in d.keys():
-          l_result.append(d[1])
+      words = s.split()
+      for i, word in enumerate(words):
+        f, l = 0, 0
+        for j in range(4, 0, -1):
+          for token in f_tokens_dict.get(word, {}).get(j, []):
+            if f != 0:
+              break
+            elif token in s:
+              f_result.append(token)
+              f = 1
+              break
+          for token in l_tokens_dict.get(word, {}).get(j, []):
+            if l != 0:
+              break
+            elif token in s:
+              l_result.append(token)
+              l = 1
+              break
 
-    result = [x for x in f_result if x in l_result]
-    
-    final[index] = result
+    final[index] = [x for x in f_result if x in l_result]
+
   return final
 
 def fl_tokenizer(doc, stopwords_ls, threshold):
